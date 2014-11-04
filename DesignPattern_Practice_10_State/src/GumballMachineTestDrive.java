1cb2d3e23c18285e007d29d636e13429793febdf
Bross <Bross@10.0.2.8>
2014-11-05 02:58:09 +0900

    2014.11.5

--- DesignPattern_Practice_10_State/src/GumballMachineTestDrive.java
+++ DesignPattern_Practice_10_State/src/GumballMachineTestDrive.java
@@ -1,17 +1,15 @@
-
 public class GumballMachineTestDrive
 {
+
 	public static void main(String[] args)
 	{
-		GumballMachine gumballMachine = new GumballMachine(5);
+		GumballMachine gumballMachine = new GumballMachine(10);
+
 		System.out.println(gumballMachine);
+
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
 		gumballMachine.insertQuarter();
-		gumballMachine.ejectQuarter();
 		gumballMachine.turnCrank();
 
 		System.out.println(gumballMachine);
@@ -20,17 +18,28 @@ public class GumballMachineTestDrive
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
-		gumballMachine.ejectQuarter();
 
 		System.out.println(gumballMachine);
 
 		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
 	}
 }
